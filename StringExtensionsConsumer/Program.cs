8129af50f839095f126019e80d60fa008e0f3dcd
Abrin Saji <abrinsaji080@gmail.com>
2026-04-12 23:52:44 -0400

    fix version

---
+++ StringExtensionsConsumer/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StringExtensionsLib;
+
+Console.WriteLine("Hello".StartsWithUpper());  // True
+Console.WriteLine("hello".StartsWithUpper());  // False
+Console.WriteLine("".StartsWithUpper());       // False
